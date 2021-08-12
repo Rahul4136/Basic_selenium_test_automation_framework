5162c74afc4b363e5b558492b7c2ceb744557734
RAHUL <rahulreddy2372@gmail.com>
2021-08-12 13:54:23 +0530

    american guy commit

--- src/test/java/qaclickacademy/SeleniumTest.java
+++ src/test/java/qaclickacademy/SeleniumTest.java
@@ -10,6 +10,7 @@ public class SeleniumTest {
 	public void BrowserAUtomation()
 	{
 		System.out.println("BrowserAUtomation");	
+		System.out.println("web automation");
 	}
 	@Test
 	public void elementsUi()
